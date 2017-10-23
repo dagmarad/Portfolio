4dc04d33275f23e419e541997d5a4d1df68bbc41
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2017-10-23 21:39:05 +0200

    js

--- dist/out.js
+++ dist/out.js
@@ -9613,6 +9613,8 @@ module.exports = __webpack_require__(18);
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(82);
 
 var _react2 = _interopRequireDefault(_react);
@@ -9623,10 +9625,66 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log("działa");
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  _reactDom2.default.render(_react2.default.createElement('app', null), document.querySelector('#app'));
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var Container = function (_React$Component) {
+    _inherits(Container, _React$Component);
+
+    function Container() {
+      _classCallCheck(this, Container);
+
+      return _possibleConstructorReturn(this, (Container.__proto__ || Object.getPrototypeOf(Container)).apply(this, arguments));
+    }
+
+    _createClass(Container, [{
+      key: 'render',
+      value: function render() {
+        return _react2.default.createElement(
+          'div',
+          null,
+          _react2.default.createElement(
+            'h1',
+            null,
+            'Kilka s\u0142\xF3w o mnie'
+          ),
+          _react2.default.createElement(
+            'p',
+            null,
+            'Uko\u0144czy\u0142am kurs z Coders Lab i teraz szukam pracy. Znam r\xF3\u017Cne fajne technologie.'
+          ),
+          _react2.default.createElement('img', null)
+        );
+      }
+    }]);
+
+    return Container;
+  }(_react2.default.Component);
+
+  var App = function (_React$Component2) {
+    _inherits(App, _React$Component2);
+
+    function App() {
+      _classCallCheck(this, App);
+
+      return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+    }
+
+    _createClass(App, [{
+      key: 'render',
+      value: function render() {
+        return _react2.default.createElement(Container, null);
+      }
+    }]);
+
+    return App;
+  }(_react2.default.Component);
+
+  _reactDom2.default.render(_react2.default.createElement(App, null), document.querySelector('#app'));
 });
 
 /***/ }),
--- js/app.jsx
+++ js/app.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("działa");
+  class Container extends React.Component{
+    render(){
+      return <div>
+      <h1>Kilka słów o mnie</h1>
+      <p>Ukończyłam kurs z Coders Lab i teraz szukam pracy. Znam różne fajne technologie.</p>
+      <img></img>
+      </div>
+    }
+  }
+  class App extends React.Component {
+    render(){
+      return <Container/>
+    }
+  }
 
-    ReactDOM.render(<app />, document.querySelector('#app'));
+
+    ReactDOM.render(<App />, document.querySelector('#app'));
 });
