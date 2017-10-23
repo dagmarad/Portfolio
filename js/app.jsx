132f4fc8456cf0f06d4059c008b68188938355fc
Dagmara Daszkiewicz <dagmara.daszkiewicz@gmail.com>
2017-10-23 20:22:34 +0200

    first commit

---
+++ js/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("działa");
+
+    ReactDOM.render(<app />, document.querySelector('#app'));
+});
